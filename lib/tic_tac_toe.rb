aadedbb0859bea141887078783bc49b8f90338b9
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-02-04 20:58:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -122,12 +122,13 @@ def play(board)
     turn(board)
   end
 
-  winner(board) == "X"
+  if winner(board) == "X"
     puts "Congratulations X!"
-  winner(board) == "O"
+  elsif winner(board) == "O"
     puts "Congratulations O!"
-  draw?(board)
+  elsif draw?(board)
     puts "Cat's Game!"
+  end
 end
 
 #winner(board)
