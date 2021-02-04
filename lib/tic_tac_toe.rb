ed71fb8039a6a0862e8de5c065e32485bb6a473b
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-02-04 20:14:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, player_character)
     display_board(board)
   else
     turn(board)
